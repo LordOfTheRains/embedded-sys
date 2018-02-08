742b1bbab93945658aa0f77226a27bc8cd0bfd01
Rain Li <yrl0001@auburn.edu>
2018-02-08 09:18:05 -0600

    final time track

--- lab1.c
+++ lab1.c
@@ -78,31 +78,35 @@ int main (int argc, char **argv) {
  * Output: None                                                          *
  * Function: Monitor Devices and process events                          *
  \***********************************************************************/
-int lastEID[MAX_NUMBER_DEVICES] = {0}; // store the last event id that device seen
-
+int processed[MAX_NUMBER_DEVICES] = {0}; // store the last event id that device seen
+Timestamp start;
+float responseTime[MAX_NUMBER_DEVICES] = {0};
 void Control(void){
   int i;
   Status LastStatus=0;
   while (1) {
+    if (Flags){
+      start = Now();
+      Flag temp = Flags;
+      Flags = 0;
+      int device = 0;
+      while (temp){
+        if (temp & 1){
+          responseTime[device] += Now() - BufferLastEvent[device].When;
+          processed[device]++;
+          DisplayEvent('d', &BufferLastEvent[device]);
+      		Server(&BufferLastEvent[device]);
+          //printf("device: %d --- lastEID: %d\n",i, lastEID[i] );
+
+        }
+        temp = temp >> 1;
+        device++;
+      }
+    }
+  }
     //printf("%10.3f   Flags = %d - \n ", Now(), Flags);
     //sleep(0); // Just to slow down to have time to see Flags
 
-  	if (CHECK_BIT(Flags, i)){ //event at device i
-      struct EventTag e = BufferLastEvent[i];
-      lastEID[i] = BufferLastEvent[i].EventID;
-  		DisplayEvent('d', &BufferLastEvent[i]);
-      printf("device: %d --- lastEID: %d\n",i, lastEID[i] );
-  		Server(&BufferLastEvent[i]);
-  		Flags &= ~(1<<i); // clear the ith bit
-  	}
-    	//printf("\n >>>>>>>>>  >>> When: %10.3f  Flags = %d\n", Now(),
-       //Flags);
-  	if (i < MAX_NUMBER_DEVICES){
-  		i++;
-  	}
-  	else
-  		i = 0;
-	}
 }
 
 
@@ -113,17 +117,18 @@ void Control(void){
 *           not yet processed (Server() function not yet called)        *
 \***********************************************************************/
 void BookKeeping(void){
-  int bufEvents = 0;
-   for (int i = 0; i < 2; i++){
-	    DisplayEvent('b', &BufferLastEvent[i]);
-      printf("device: [%d] --- lastEID: %d\n",i, lastEID[i] );
-      bufEvents += 99 - lastEID[i] ;
-	     // if (BufferLastEvent[i] != NULL){
-	     //    bufEvents++;
-	     //  }
+  Timestamp done = Now();
+  int missed = 0;
+  int total = 0;
+   for (int i = 0; i < MAX_NUMBER_DEVICES ; i++){
+      int numEvent = BufferLastEvent[i].EventID;
+      if (numEvent > 0){
+  	    DisplayEvent('b', &BufferLastEvent[i]);
+        missed += numEvent - processed[i] + 1 ;
+        printf("device: [%d] --- avg response time: %d\n",i,  responseTime[i]/numEvent );
+        printf("device: [%d] --- processed: %d\n",i, processed[i] );
+      }
   }
-  printf("Buffered Events: %d\n", bufEvents);
+  printf("total turn around time %10.3f\n", done);
   printf("\n >>>>>> Done\n");
 }
-
-
