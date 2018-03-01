3302f51e815c1a049911bae21088b795f28ba976
Rain Li <yrl0001@auburn.edu>
2018-03-01 08:20:41 -0600

    added lab1 code to lab 2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -66,17 +66,43 @@ int main (int argc, char **argv) {
 
    if (Initialization(argc,argv)){
      Control();
-   } 
+   }
 } /* end of main function */
 
+
+int processed[MAX_NUMBER_DEVICES] = {0}; // store the last event id that device seen
+
+double turnaround[MAX_NUMBER_DEVICES] = {0};
+double responseTime[MAX_NUMBER_DEVICES] = {0};
 /***********************************************************************\
  * Input : none                                                          *
  * Output: None                                                          *
  * Function: Monitor Devices and process events (written by students)    *
  \***********************************************************************/
 void Control(void){
-
-  while (1);
+  int i;
+  while (1){
+    if (Flags){
+      unsigned temp = Flags; //temp is negative sometimes even if flag is not
+      //printf("Flags = %d - \n ",Flags);
+      //printf("temp = %d...... \n ",temp);
+      Flags = 0;
+      int device = 0;
+      while (temp){
+        if (temp & 1){
+           DisplayEvent('d', &BufferLastEvent[device]);
+          responseTime[device] += Now() - BufferLastEvent[device].When;
+      		Server(&BufferLastEvent[device]);
+          turnaround[device] += Now() - BufferLastEvent[device].When;
+          processed[device]++;
+        }
+        temp = temp >> 1;
+        device++;
+        //printf("temp = %d - \n ",temp);
+      }
+    }
+  }
+  }
 
 }
 
@@ -101,13 +127,19 @@ void InterruptRoutineHandlerDevice(void){
 \***********************************************************************/
 void BookKeeping(void){
   // For EACH device, print out the following metrics :
-  // 1) the percentage of missed events, 2) the average response time, and 
+  // 1) the percentage of missed events, 2) the average response time, and
   // 3) the average turnaround time.
   // Print the overall averages of the three metrics 1-3 above
+  int i;
+  printf("\nStart BookKeeping ...\n");
+  printf("\nDevice\tmissed\tavg response time\tavg turnaround time\tlast event\n");
+  for (i = 0; i < MAX_NUMBER_DEVICES ; i++){
+    int numEvent = BufferLastEvent[i].EventID + 1;
+    if (numEvent > 0){
+      float missed = numEvent - processed[i];
+      printf("%d\t%5.2f",i,missed/numEvent*100.0);
+      printf("\t\t%lf\t\t%lf\t%d\n",responseTime[i]/numEvent,turnaround[i]/numEvent,numEvent );
+      //printf("device [%d]:missed %d percent events with avg response time: %lf\n",i, missed/numEvent*100 ,responseTime[i]/numEvent );
+    }
+  }
 }
-
-
-
-
-
-
